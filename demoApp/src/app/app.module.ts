2d0b2578caccb7a271d244a9747e553ca715ba4e
hanu <hanuwebui@gmail.com>
2020-11-24 06:36:19 +0530

    updated components

--- demoApp/src/app/app.module.ts
+++ demoApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
+import { FiveComponent } from './five/five.component';
+import { SixComponent } from './six/six.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent,
+    FiveComponent,
+    SixComponent
   ],
   imports: [
     BrowserModule,
